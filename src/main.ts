62e1bf209e30b21956ba5120cfdefeac424b0503
Vladyslav Ivashko <vladyslavivashko@MacBook-Air.local>
2025-09-02 12:27:09 +0300

    chore: add cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(cookieParser());
+  app.enableCors({ origin: process.env.WEB_APP_URL ?? '' });
   await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
